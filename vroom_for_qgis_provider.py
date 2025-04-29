ae8f27327503a97fdf2f34632032f3b66ac7d298
Suddber <suddb@posteo.de>
2025-04-29 14:00:25 +0200

    remaining comments from Plugin Builder deleted (#2)

--- vroom_for_qgis_provider.py
+++ vroom_for_qgis_provider.py
@@ -37,55 +37,31 @@ from .vroom_for_qgis_algorithm import VROOM_for_QGISAlgorithm
 class VROOM_for_QGISProvider(QgsProcessingProvider):
 
     def __init__(self):
-        """
-        Default constructor.
-        """
+        
         QgsProcessingProvider.__init__(self)
 
     def unload(self):
-        """
-        Unloads the provider. Any tear-down steps required by the provider
-        should be implemented here.
-        """
+        
         pass
 
     def loadAlgorithms(self):
-        """
-        Loads all algorithms belonging to this provider.
-        """
+        
         self.addAlgorithm(VROOM_for_QGISAlgorithm())
         # add additional algorithms here
         # self.addAlgorithm(MyOtherAlgorithm())
 
     def id(self):
-        """
-        Returns the unique provider id, used for identifying the provider. This
-        string should be a unique, short, character only string, eg "qgis" or
-        "gdal". This string should not be localised.
-        """
+        
         return 'VROOM for QGIS'
 
     def name(self):
-        """
-        Returns the provider name, which is used to describe the provider
-        within the GUI.
-
-        This string should be short (e.g. "Lastools") and localised.
-        """
+        
         return self.tr('VROOM for QGIS')
 
     def icon(self):
-        """
-        Should return a QIcon which is used for your provider inside
-        the Processing toolbox.
-        """
+        
         return QgsProcessingProvider.icon(self)
 
     def longName(self):
-        """
-        Returns the a longer version of the provider name, which can include
-        extra details such as version numbers. E.g. "Lastools LIDAR tools
-        (version 2.2.1)". This string should be localised. The default
-        implementation returns the same string as name().
-        """
+        
         return self.name()
